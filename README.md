dcfc257e561b3b127928430fb878133f2f9b90d2
Richard Davies <richard@catchdesign.co.nz>
2018-12-13 14:26:07 +1300

    Init

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 An esdoc module.
 
 Rewrites UMD style modules so esdoc can parse them.
+
+Modules will require an esdoc compatible docblock comment to correctly render usage information in esdoc.
---
+++ index.js
@@ -0,0 +1,50 @@
+var through = require('through2');
+var ESDoc = require('esdoc').default;
+var gutil = require('gulp-util');
+
+var fs = require('fs'),
+    path = require('path');
+
+const PLUGIN_NAME = 'gulp-esdoccr';
+
+function gulpESDoc(config) {
+  if (!config) {
+    var cfgPath = path.join(process.cwd(), 'esdoc.json'),
+        data;
+
+    if (fs.existsSync(cfgPath)) {
+      data = fs.readFileSync(cfgPath, { encoding: 'utf8' });
+      config = !data ? {} : JSON.parse(data);
+    } else {
+      config  = {};
+    }
+  }
+
+  var transform = function(file, encode, callback) {
+
+    if (!config.destination) {
+      this.emit('error', new gutil.PluginError(PLUGIN_NAME, 'Must specify config.destination'));
+      return callback();
+    }
+
+    if (file.isStream()) {
+      this.emit('error', new gutil.PluginError(PLUGIN_NAME, 'Streaming not supported'));
+      return callback();
+    }
+
+    config.source = config.source || file.path;
+
+    callback();
+  };
+
+  var flush = function(callback) {
+    ESDoc.generate(config);
+    callback();
+  };
+
+  return through.obj(transform, flush);
+
+}
+
+// Export
+module.exports = gulpESDoc;
